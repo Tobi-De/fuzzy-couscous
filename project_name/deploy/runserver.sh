252a1953ca809f48915b437f7d6e067be7f75429
Tobi-De <tobidegnon@protonmail.com>
2022-11-21 13:18:42 +0100

    inital commit

--- project_name/deploy/runserver.sh
+++ project_name/deploy/runserver.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+tailwindcss -i {{ project_name }}/static/css/style.css -o {{ project_name }}/static/css/output.css --minify
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py makesuperuser
