5d55c7cd4b80553b2c74e82698a1090783a2e15e
Tobi-De <tobidegnon@protonmail.com>
2022-11-21 13:18:42 +0100

    inital commit

---
+++ project_name/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+        '{{ project_name }}/templates/**/*.html'
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
