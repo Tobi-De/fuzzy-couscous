8a6ed8230f70bd54f6d9bfe24214b0693d2606d2
Tobi DEGNON <tobidegnon@proton.me>
2023-11-16 17:11:26 +0100

    more stuff for new base

--- templates/base/project_name/docker/entrypoint.sh
+++ templates/base/project_name/docker/entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py makesuperuser
-gunicorn config.wsgi --config="docker/gunicorn.conf.py"
+granian --interface wsgi config.wsgi:application --host 0.0.0.0 --port 80 --workers 4
